ec58e2c499591dfce22fdc7d0fdd3b3799776461
eydisagusts <eydis15@gmail.com>
2024-04-22 21:57:30 +0000

    Quiz App

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
+
+import 'package:first_app/quiz.dart';
 
 void main() {
-  runApp(
-     const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 33, 5, 109),
-          Color.fromARGB(255, 68, 21, 149),
-        )
-        ),
-      ),
-    );
+  runApp(const Quiz());
 }
-
